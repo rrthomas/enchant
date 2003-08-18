13f4b5c42613d7c42d3f55ebe018f8bbf6cc4645
Dom Lachowicz <domlachowicz@gmail.com>
2003-08-18 12:44:40 +0000

    fix dllimport/export thing for schurro
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/enchant/trunk@20797 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/enchant.c
+++ src/enchant.c
@@ -28,9 +28,6 @@
  * do so, delete this exception statement from your version.
  */
 
-/* for proper DLL import/export semantics on Win32 */
-#define _ENCHANT_BUILD 1
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
