b00e86752081aad6e6285b2d9551f81b34a0b8f6
Dom Lachowicz <domlachowicz@gmail.com>
2010-01-11 12:40:00 +0000

    Bug 12567 - the ispell sources aren't licensed under the LGPL
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/enchant/trunk@28601 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/ispell/hash.cpp
+++ src/ispell/hash.cpp
@@ -1,33 +1,4 @@
 /* vim: set sw=8: -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-/* enchant
- * Copyright (C) 2003 Dom Lachowicz
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02110-1301, USA.
- *
- * In addition, as a special exception, Dom Lachowicz
- * gives permission to link the code of this program with
- * non-LGPL Spelling Provider libraries (eg: a MSFT Office
- * spell checker backend) and distribute linked combinations including
- * the two.  You must obey the GNU Lesser General Public License in all
- * respects for all of the code used other than said providers.  If you modify
- * this file, you may extend this exception to your version of the
- * file, but you are not obligated to do so.  If you do not wish to
- * do so, delete this exception statement from your version.
- */
-
 /*
  * hash.c - a simple hash function for ispell
  *
