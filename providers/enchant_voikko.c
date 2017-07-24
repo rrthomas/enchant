rrthomas/enchant

commit 38f7f06e7ed8d1990f3fc76c40cf1e55875cfa3d

Remove enchant provider free_string_list method
Assume a malloc-allocated char **.

Voikko used an internally-allocated char **, so copy it before passing it
back, as all the other providers have to.

In the process of fixing some tests, reactivate some disabled tests.

providers/enchant_voikko.c
@@ -63,10 +63,16 @@ static char **
 voikko_dict_suggest (EnchantDict * me, const char *const word,
 		     size_t len _GL_UNUSED_PARAMETER, size_t * out_n_suggs)
 {
-	char **sugg_arr = voikkoSuggestCstr((struct VoikkoHandle *)me->user_data, word);
-	if (sugg_arr == NULL)
+	char **voikko_sugg_arr = voikkoSuggestCstr((struct VoikkoHandle *)me->user_data, word);
+	if (voikko_sugg_arr == NULL)
 		return NULL;
-	for (*out_n_suggs = 0; sugg_arr[*out_n_suggs] != NULL; (*out_n_suggs)++);
+	for (*out_n_suggs = 0; voikko_sugg_arr[*out_n_suggs] != NULL; (*out_n_suggs)++);
+
+	char **sugg_arr = calloc(sizeof (char *), *out_n_suggs);
+	for (size_t i = 0; i < *out_n_suggs; i++) {
+		sugg_arr[i] = strdup (voikko_sugg_arr[i]);
+	}
+	voikkoFreeCstrArray (voikko_sugg_arr);
 	return sugg_arr;
 }
 
@@ -142,13 +148,6 @@ voikko_provider_list_dicts (EnchantProvider * me _GL_UNUSED_PARAMETER,
 }
 
 static void
-voikko_provider_free_string_list (EnchantProvider * me _GL_UNUSED_PARAMETER,
-				  char **str_list)
-{
-	voikkoFreeCstrArray (str_list);
-}
-
-static void
 voikko_provider_dispose (EnchantProvider * me)
 {
 	free (me);
@@ -181,7 +180,6 @@ init_enchant_provider (void)
 	provider->identify = voikko_provider_identify;
 	provider->describe = voikko_provider_describe;
 	provider->list_dicts = voikko_provider_list_dicts;
-	provider->free_string_list = voikko_provider_free_string_list;
 
 	return provider;
 }

providers/enchant_aspell.c
@@ -237,12 +237,6 @@ aspell_provider_list_dicts (EnchantProvider * me _GL_UNUSED_PARAMETER,
 }
 
 static void
-aspell_provider_free_string_list (EnchantProvider * me _GL_UNUSED_PARAMETER, char **str_list)
-{
-	g_strfreev (str_list);
-}
-
-static void
 aspell_provider_dispose (EnchantProvider * me)
 {
 	g_free (me);
@@ -272,7 +266,6 @@ init_enchant_provider (void)
 	provider->identify = aspell_provider_identify;
 	provider->describe = aspell_provider_describe;
 	provider->list_dicts = aspell_provider_list_dicts;
-	provider->free_string_list = aspell_provider_free_string_list;
 
 	return provider;
 }

providers/enchant_hspell.c
@@ -251,12 +251,6 @@ hspell_provider_dictionary_exists (struct str_enchant_provider * me,
 }
 
 static void
-hspell_provider_free_string_list (EnchantProvider * me  _GL_UNUSED_PARAMETER, char **str_list)
-{
-	g_strfreev (str_list);
-}
-
-static void
 hspell_provider_dispose (EnchantProvider * me)
 {
 	g_free (me);
@@ -289,7 +283,6 @@ init_enchant_provider (void)
 	provider->identify = hspell_provider_identify;
 	provider->describe = hspell_provider_describe;
 	provider->list_dicts = hspell_provider_list_dicts;
-	provider->free_string_list = hspell_provider_free_string_list;
 
 	return provider;
 }

providers/enchant_hunspell.cpp
@@ -405,12 +405,6 @@ hunspell_provider_list_dicts (EnchantProvider * me _GL_UNUSED_PARAMETER,
 	return dictionary_list;
 }
 
-static void
-hunspell_provider_free_string_list (EnchantProvider * me _GL_UNUSED_PARAMETER, char **str_list)
-{
-	g_strfreev (str_list);
-}
-
 static EnchantDict *
 hunspell_provider_request_dict(EnchantProvider * me _GL_UNUSED_PARAMETER, const char *const tag)
 {
@@ -497,7 +491,6 @@ init_enchant_provider (void)
 	provider->dictionary_exists = hunspell_provider_dictionary_exists;
 	provider->identify = hunspell_provider_identify;
 	provider->describe = hunspell_provider_describe;
-	provider->free_string_list = hunspell_provider_free_string_list;
 	provider->list_dicts = hunspell_provider_list_dicts;
 
 	return provider;

providers/enchant_zemberek.cpp
@@ -216,12 +216,6 @@ zemberek_provider_describe (EnchantProvider * me _GL_UNUSED_PARAMETER)
 	return "Zemberek Provider";
 }
 
-static void
-zemberek_provider_free_string_list (EnchantProvider * me _GL_UNUSED_PARAMETER, char **str_list)
-{
-	g_strfreev (str_list);
-}
-
 static char **
 zemberek_provider_list_dicts (EnchantProvider * me _GL_UNUSED_PARAMETER,
 			      size_t * out_n_dicts)
@@ -254,7 +248,6 @@ init_enchant_provider(void)
     provider->identify = zemberek_provider_identify;
     provider->describe = zemberek_provider_describe;
     provider->list_dicts = zemberek_provider_list_dicts;
-    provider->free_string_list = zemberek_provider_free_string_list;
 
     return provider;
 }

src/enchant-provider.h
@@ -135,10 +135,6 @@ struct str_enchant_provider
 	/* returns utf8*/
 	const  char * (*describe) (struct str_enchant_provider * me);
 
-	/* frees string lists returned by list_dicts and dict->suggest */
-	void (*free_string_list) (struct str_enchant_provider * me,
-				  char **str_list);
-
 	char ** (*list_dicts) (struct str_enchant_provider * me,
 							   size_t * out_n_dicts);
 };

src/lib.c
@@ -428,10 +428,9 @@ enchant_session_clear_error (EnchantSession * session)
 /********************************************************************************/
 
 static void
-enchant_provider_free_string_list (EnchantProvider * provider, char ** string_list)
+enchant_free_string_list (char ** string_list)
 {
-	if (provider && provider->free_string_list)
-		(*provider->free_string_list) (provider, string_list);
+	g_strfreev (string_list);
 }
 
 void
@@ -597,7 +596,7 @@ enchant_dict_suggest (EnchantDict * dict, const char *const word,
 			if(dict_suggs)
 				{
 					suggsT = enchant_dict_get_good_suggestions(dict, dict_suggs, n_dict_suggs, &n_suggsT);
-					enchant_provider_free_string_list (session->provider, dict_suggs);
+					enchant_free_string_list (dict_suggs);
 					dict_suggs = suggsT;
 					n_dict_suggs = n_suggsT;
 				}
@@ -610,7 +609,7 @@ enchant_dict_suggest (EnchantDict * dict, const char *const word,
 			if(pwl_suggs)
 				{
 					suggsT = enchant_dict_get_good_suggestions(dict, pwl_suggs, n_pwl_suggs, &n_suggsT);
-					enchant_pwl_free_string_list (session->personal, pwl_suggs);
+					enchant_free_string_list (pwl_suggs);
 					pwl_suggs = suggsT;
 					n_pwl_suggs = n_suggsT;
 				}
@@ -1413,7 +1412,7 @@ enchant_broker_list_dicts (EnchantBroker * broker,
 							}
 						}
 
-					enchant_provider_free_string_list (provider, dicts);
+					enchant_free_string_list (dicts);
 				}
 		}
 
@@ -1483,7 +1482,7 @@ enchant_provider_dictionary_exists (EnchantProvider * provider,
 						exists = 1;
 				}
 
-			enchant_provider_free_string_list (provider, dicts);
+			enchant_free_string_list (dicts);
 		}
 	else if (provider->request_dict)
 		{

src/pwl.c
@@ -763,12 +763,6 @@ static void enchant_pwl_suggest_cb(char* match,EnchantTrieMatcher* matcher)
 
 }
 
-void enchant_pwl_free_string_list(EnchantPWL *pwl _GL_UNUSED_PARAMETER,
-				  char** string_list)
-{
-	g_strfreev(string_list);
-}
-
 static void enchant_trie_free(EnchantTrie* trie)
 {
 	/* Don't try to free NULL or the EOSTrie pointer */

commit 6c3de822c6f410b5995979bac6b2bf9536c412c2

Fix incorrect return value in Voikko’s suggest routine; actually test Voikko!
Commit 38f7f06e7 introduced this bug: the returned array of suggestions was
not NULL-terminated. This caused a crash in the tests, uncovered by Travis
on macOS, as on Ubuntu we didn’t have a dictionary for Voikko.

Add a Finnish dictionary for Voikko to the Travis setup for Ubuntu.

.travis.yml
@@ -13,6 +13,7 @@ addons:
       - hspell
       - libhunspell-dev
       - libvoikko-dev
+      - voikko-fi
       - aspell-en
       - libunittest++-dev
       - valgrind

providers/enchant_voikko.c
@@ -67,7 +67,7 @@ voikko_dict_suggest (EnchantDict * me, const char *const word,
 		return NULL;
 	for (*out_n_suggs = 0; voikko_sugg_arr[*out_n_suggs] != NULL; (*out_n_suggs)++);
 
-	char **sugg_arr = calloc(sizeof (char *), *out_n_suggs);
+	char **sugg_arr = calloc(sizeof (char *), *out_n_suggs + 1);
 	for (size_t i = 0; i < *out_n_suggs; i++) {
 		sugg_arr[i] = strdup (voikko_sugg_arr[i]);
 	}