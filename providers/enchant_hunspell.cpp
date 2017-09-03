87b36fd87d8ecc894850d82eed33f48d6c429cad
Pacho Ramos <pacho@gentoo.org>
2017-09-03 11:05:42 +0200

    Fix hunspell 1.5 support

--- providers/enchant_hunspell.cpp
+++ providers/enchant_hunspell.cpp
@@ -312,7 +312,7 @@ HunspellChecker::requestDictionary(const char *szLang)
 	if(hunspell == NULL){
 		return false;
 	}
-	char *enc = hunspell->get_dic_encoding();
+	const char *enc = hunspell->get_dic_encoding();
 
 	m_translate_in = g_iconv_open(enc, "UTF-8");
 	m_translate_out = g_iconv_open("UTF-8", enc);
