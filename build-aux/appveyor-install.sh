07c4f3fb2056536853fcc237c6d05d481d38fbea
Reuben Thomas <rrt@sc3d.org>
2020-11-24 12:45:34 +0000

    Switch from Travis to AppVeyor

--- build-aux/appveyor-install.sh
+++ build-aux/appveyor-install.sh
@@ -4,37 +4,19 @@
 # Get mingw type, if any, from MSYSTEM
 case $MSYSTEM in
     MINGW32)
-        MINGW_BITS=i686
+        MINGW_ARCH=i686
         PREFIX=/mingw32
         ;;
     MINGW64)
-        MINGW_BITS=x86_64
+        MINGW_ARCH=x86_64
         PREFIX=/mingw64
-        ;;
-    MSYS)
-        PREFIX=/usr
         ;;
 esac
 
 # GLib
-if test -n "$MINGW_BITS"; then
-    pacman --noconfirm -S mingw-w64-$MINGW_BITS-glib2
-fi
-pacman --noconfirm -S glib2-devel
+pacman --noconfirm -S mingw-w64-$MINGW_ARCH-glib2 glib2-devel mingw-w64-$MINGW_ARCH-hunspell mingw-w64-$MINGW_ARCH-hunspell-en
 
-# Hunspell dictionary (only need one flavour)
-wget http://repo.msys2.org/mingw/x86_64/mingw-w64-x86_64-hunspell-en-2016.11.20-2-any.pkg.tar.xz
-pacman --noconfirm -U mingw-w64-x86_64-hunspell-en-2016.11.20-2-any.pkg.tar.xz
-
-# Hunspell
-wget https://github.com/hunspell/hunspell/archive/v1.6.0.tar.gz
-tar zxvf v1.6.0.tar.gz || true # Error in unpacking (symlink README before file README.md)
-cd hunspell-1.6.0
-ln -s README.md README && autoreconf -vfi
-./configure --prefix=$PREFIX && make && make install
-cd ..
-
-# UnitTest++
+# UnitTest++ is not packaged in mingw
 wget https://github.com/unittest-cpp/unittest-cpp/releases/download/v1.6.1/unittest-cpp-1.6.1.tar.gz
 tar zxvf unittest-cpp-1.6.1.tar.gz
 cd unittest-cpp-1.6.1
