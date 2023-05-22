be51f372c4785e27299dae7824b050ed7bf3dfd4
Reuben Thomas <rrt@sc3d.org>
2023-05-22 16:29:16 +0100

    Remove PWL suggestion code

--- src/pwl.h
+++ src/pwl.h
@@ -45,9 +45,6 @@ EnchantPWL* enchant_pwl_init_with_file(const char * file);
 void enchant_pwl_add(EnchantPWL * me, const char *const word, ssize_t len);
 void enchant_pwl_remove(EnchantPWL * me, const char *const word, ssize_t len);
 int enchant_pwl_check(EnchantPWL * me, const char *const word, ssize_t len);
-/*gives the best set of suggestions from pwl that are at least as good as the given suggs*/
-char** enchant_pwl_suggest(EnchantPWL *me, const char *const word,
-			   ssize_t len, char ** suggs, size_t* out_n_suggs);
 void enchant_pwl_free(EnchantPWL* me);
 
 #ifdef __cplusplus
