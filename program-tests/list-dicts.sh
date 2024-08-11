5f877580db4e36de214f6d755f73e116d81772b7
Reuben Thomas <rrt@sc3d.org>
2024-08-11 21:46:11 +0100

    Fold enchant-lsmod into enchant
    Resurrect zero-arguments test in program-tests.

---
+++ program-tests/list-dicts.sh
@@ -0,0 +1,3 @@
+# Running enchant --list-dicts with some providers and dictionaries
+# installed does some useful testing.
+enchant_no_output_test --list-dicts
--- program-tests/run-enchant-lsmod.sh
+++
@@ -1,3 +0,0 @@
-# Running enchant-lsmod -list-dicts with some providers and dictionaries
-# installed does some useful testing.
-enchant_lsmod_test -list-dicts
