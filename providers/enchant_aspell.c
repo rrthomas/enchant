db6d49384e12e1ed4700ec5678678e48a3ffaca4
Reuben Thomas <rrt@sc3d.org>
2025-06-17 21:45:48 +0100

    Aspell provider: fix crash in list_dicts (fix #421)
    Thanks very much to @chenrui333 for the analysis and suggested fix.
    
    I prefer a more conservative fix that restores a couple of missing lines
    that commit 352cc86 removed.

--- providers/enchant_aspell.c
+++ providers/enchant_aspell.c
@@ -155,6 +155,8 @@ aspell_provider_list_dicts (EnchantProvider * me _GL_UNUSED,
 	while ( (entry = aspell_dict_info_enumeration_next(dels)) != 0)
 		(*out_n_dicts)++;
 
+        delete_aspell_dict_info_enumeration (dels);
+        dels = aspell_dict_info_list_elements (dlist);
 	char ** out_list = g_new0 (char *, *out_n_dicts + 1);
 	if (out_list)
 		for (size_t i = 0; i < *out_n_dicts; i++) {
