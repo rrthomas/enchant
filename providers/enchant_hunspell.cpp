rrthomas/enchant

commit 372deba515edf0dbc4616a1db6b4a0a1e91f0666

Providers: check return value of g_new0 every time
Also some minor formatting fixes and one or two other minor tweaks.

providers/enchant_hunspell.cpp
@@ -62,6 +62,8 @@ static char *do_iconv(GIConv conv, const char *word) {
 	size_t len_in = strlen(in);
 	size_t len_out = len_in * 3;
 	char *out_buf = g_new0(char, len_out + 1);
+	if (out_buf == NULL)
+		return NULL;
 	char *out = out_buf;
 	size_t result = g_iconv(conv, &in, &len_in, &out, &len_out);
 	if (static_cast<size_t>(-1) == result)
@@ -157,18 +159,18 @@ HunspellChecker::suggestWord(const char* const utf8Word, size_t len, size_t *nsu
 
 	std::vector<std::string> sugMS = hunspell->suggest(out);
 	g_free(out);
-	*nsug = sugMS.size();
-	if (*nsug > 0) {
-		char **sug = g_new0 (char *, *nsug + 1);
-		for (size_t i=0, j=0; i<*nsug; i++) {
+	char **sug = g_new0 (char *, *nsug + 1);
+	if (sug) {
+		for (size_t i = 0, j = 0; i < *nsug; i++) {
 			const char *in = sugMS[i].c_str();
 			out = do_iconv(m_translate_out, in);
 			if (out != NULL)
 				sug[j++] = out;
 		}
-		return sug;
-	}
-	return nullptr;
+		*nsug = sugMS.size();
+	} else
+		*nsug = 0;
+	return sug;
 }
 
 void
@@ -378,9 +380,12 @@ hunspell_provider_list_dicts (EnchantProvider * me, size_t * out_n_dicts)
 
 	// Convert vector to array of pointers and return.
 	char ** dictionary_list = g_new0 (char *, dicts.size() + 1);
-	for (size_t i = 0; i < dicts.size(); i++)
-		dictionary_list[i] = g_strdup (dicts[i].c_str());
-	*out_n_dicts = dicts.size ();
+	if (dictionary_list) {
+		for (size_t i = 0; i < dicts.size(); i++)
+			dictionary_list[i] = g_strdup (dicts[i].c_str());
+		*out_n_dicts = dicts.size ();
+	} else
+		*out_n_dicts = 0;
 	return dictionary_list;
 }
 

providers/applespell_checker.mm
@@ -118,11 +118,9 @@ char **AppleSpellChecker::NSArrayToCArray (NSArray<NSString *> *array, size_t *n
 		if (result) {
 			for (unsigned int i = 0; i < count; i++) {
 				NSString *str = [array objectAtIndex:i];
-
 				result[*nresult] = g_strdup ([str UTF8String]);
-
 				if (result[*nresult])
-					*nresult = *nresult + 1;
+					(*nresult)++;
 			}
 		}
 	}

providers/enchant_aspell.c
@@ -1,6 +1,6 @@
 /* enchant
  * Copyright (C) 2003,2004 Dom Lachowicz
- * Copyright (C) 2017-2024 Reuben Thomas
+ * Copyright (C) 2017-2025 Reuben Thomas
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -76,28 +76,25 @@ aspell_dict_suggest (EnchantDict * me, const char *const word,
 	g_free(normalizedWord);
 
 	char **sugg_arr = NULL;
-	if (word_list)
-		{
-			AspellStringEnumeration *suggestions = aspell_word_list_elements (word_list);
-			if (suggestions)
-				{
-					size_t n_suggestions = aspell_word_list_size (word_list);
-					*out_n_suggs = n_suggestions;
+	if (word_list) {
+		AspellStringEnumeration *suggestions = aspell_word_list_elements (word_list);
+		if (suggestions) {
+			size_t n_suggestions = aspell_word_list_size (word_list);
 
-					if (n_suggestions)
-						{
-							sugg_arr = g_new0 (char *, n_suggestions + 1);
-
-							for (size_t i = 0; i < n_suggestions; i++)
-								{
-									const char *sugg = aspell_string_enumeration_next (suggestions);
-									if (sugg)
-										sugg_arr[i] = g_strdup (sugg);
-								}
-						}
-					delete_aspell_string_enumeration (suggestions);
-				}
+			if (n_suggestions) {
+				*out_n_suggs = n_suggestions;
+				sugg_arr = g_new0 (char *, n_suggestions + 1);
+				if (sugg_arr)
+					for (size_t i = 0; i < n_suggestions; i++) {
+						const char *sugg = aspell_string_enumeration_next (suggestions);
+						if (sugg)
+							sugg_arr[i] = g_strdup (sugg);
+					}
+			} else
+				*out_n_suggs = 0;
+			delete_aspell_string_enumeration (suggestions);
 		}
+	}
 
 	return sugg_arr;
 }
@@ -120,12 +117,11 @@ aspell_provider_request_dict (EnchantProvider * me, const char *const tag)
 	AspellCanHaveError *spell_error = new_aspell_speller (spell_config);
 	delete_aspell_config (spell_config);
 
-	if (aspell_error_number (spell_error) != 0)
-		{
-			enchant_provider_set_error (me, aspell_error_message (spell_error));
-			delete_aspell_can_have_error(spell_error);
-			return NULL;
-		}
+	if (aspell_error_number (spell_error) != 0) {
+		enchant_provider_set_error (me, aspell_error_message (spell_error));
+		delete_aspell_can_have_error(spell_error);
+		return NULL;
+	}
 
 	AspellSpeller *manager = to_aspell_speller (spell_error);
 
@@ -161,10 +157,13 @@ aspell_provider_list_dicts (EnchantProvider * me _GL_UNUSED,
 		(*out_n_dicts)++;
 
 	char ** out_list = g_new0 (char *, *out_n_dicts + 1);
-	for (size_t i = 0; i < *out_n_dicts; i++) {
-		entry = aspell_dict_info_enumeration_next (dels);
-		out_list[i] = g_strdup (entry->name);
-	}
+	if (out_list)
+		for (size_t i = 0; i < *out_n_dicts; i++) {
+			entry = aspell_dict_info_enumeration_next (dels);
+			out_list[i] = g_strdup (entry->name);
+		}
+	else
+		*out_n_dicts = 0;
 
 	delete_aspell_dict_info_enumeration (dels);
 	delete_aspell_config (spell_config);

providers/enchant_hspell.c
@@ -1,7 +1,7 @@
 /* enchant
  * Copyright (C) 2003 Yaacov Zamir
  * Copyright (C) 2004 Dom Lachowicz
- * Copyright (C) 2017-2024 Reuben Thomas
+ * Copyright (C) 2017-2025 Reuben Thomas
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -53,19 +53,15 @@
 static gchar **
 corlist2strv (struct corlist *cl, size_t nb_sugg)
 {
-	char **sugg_arr = NULL;
-	if (nb_sugg > 0)
-		{
-			sugg_arr = g_new0 (char *, nb_sugg + 1);
-			for (size_t i = 0; i < nb_sugg; i++)
-				{
-					gsize len;
-					const char *sugg = corlist_str (cl, i);
-					if (sugg)
-						sugg_arr[i] = g_convert (sugg,
-									 strlen (sugg),
-									 "utf-8", "iso8859-8", NULL, &len, NULL);
-				}
+	char **sugg_arr = g_new0 (char *, nb_sugg + 1);
+	if (sugg_arr)
+		for (size_t i = 0; i < nb_sugg; i++) {
+			gsize len;
+			const char *sugg = corlist_str (cl, i);
+			if (sugg)
+				sugg_arr[i] = g_convert (sugg,
+							 strlen (sugg),
+							 "utf-8", "iso8859-8", NULL, &len, NULL);
 		}
 
 	return sugg_arr;
@@ -188,11 +184,10 @@ hspell_provider_request_dict (EnchantProvider * me, const char *const tag)
 	struct dict_radix *hspell_dict = NULL;
 	int dict_flag = hspell_init (&hspell_dict, HSPELL_OPT_DEFAULT);
 
-	if (dict_flag != 0 || !hspell_dict)
-		{
-			enchant_provider_set_error (me, "cannot get requested dictionary");
-			return NULL;
-		}
+	if (dict_flag != 0 || !hspell_dict) {
+		enchant_provider_set_error (me, "cannot get requested dictionary");
+		return NULL;
+	}
 
 	EnchantDict *dict = enchant_broker_new_dict (me->owner);
 	dict->user_data = (void *) hspell_dict;
@@ -220,21 +215,22 @@ hspell_provider_list_dicts (EnchantProvider * me, size_t * out_n_dicts)
 	GSList * dict_files = hspell_provider_enum_dict_files (user_dict_dir);
 	guint n_user_dicts = g_slist_length (dict_files);
 	char ** out_list = g_new0 (char *, n_user_dicts + 3);
-	for (guint i = 0; i < n_user_dicts; i++) {
-		gchar * dict_file = g_slist_nth_data (dict_files, i);
-		gchar * basename = g_path_get_basename (dict_file);
-		gchar * tag_end = g_strrstr (basename, HSPELL_MAIN_SUFFIX);
-		if (tag_end != NULL) {
-			*tag_end = '\0';
-			out_list[(*out_n_dicts)++] = basename;
-		} else
-			g_free (basename);
-	}
+	if (out_list)
+		for (guint i = 0; i < n_user_dicts; i++) {
+			gchar * dict_file = g_slist_nth_data (dict_files, i);
+			gchar * basename = g_path_get_basename (dict_file);
+			gchar * tag_end = g_strrstr (basename, HSPELL_MAIN_SUFFIX);
+			if (tag_end != NULL) {
+				*tag_end = '\0';
+				out_list[(*out_n_dicts)++] = basename;
+			} else
+				g_free (basename);
+		}
 	g_free (user_dict_dir);
 	g_slist_free_full (dict_files, g_free);
 
 	const char * dictionary_path = hspell_get_dictionary_path();
-	if(dictionary_path && *dictionary_path && g_file_test (dictionary_path, G_FILE_TEST_EXISTS)) {
+	if(out_list && dictionary_path && *dictionary_path && g_file_test (dictionary_path, G_FILE_TEST_EXISTS)) {
 		out_list[(*out_n_dicts)++] = g_strdup ("he");
 		out_list[(*out_n_dicts)++] = g_strdup ("he_IL");
 	}

providers/enchant_nuspell.cpp
@@ -1,6 +1,7 @@
 /* enchant
  * Copyright (C) 2022 Dimitrij Mijoski
  * Copyright (C) 2020 Sander van Geloven
+ * Copyright (C) 2024-2025 Reuben Thomas
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -69,14 +70,13 @@ static char** nuspell_dict_suggest(EnchantDict* me, const char* const word,
 	    UniquePtr(g_utf8_normalize(word, len, G_NORMALIZE_NFC), g_free);
 	auto suggestions = vector<string>();
 	dict->suggest(normalized_word.get(), suggestions);
-	if (empty(suggestions)) {
-		*out_n_suggs = 0;
-		return nullptr;
-	}
 	char** sug_list = g_new0(char*, size(suggestions) + 1);
-	transform(begin(suggestions), end(suggestions), sug_list,
-	          [](const string& sug) { return g_strdup(sug.c_str()); });
-	*out_n_suggs = size(suggestions);
+	if (sug_list) {
+		transform(begin(suggestions), end(suggestions), sug_list,
+			  [](const string& sug) { return g_strdup(sug.c_str()); });
+		*out_n_suggs = size(suggestions);
+	} else
+		*out_n_suggs = 0;
 	return sug_list;
 }
 // End EnchantDict functions
@@ -152,10 +152,6 @@ nuspell_provider_list_dicts(EnchantProvider* me,
 	auto dirs = nuspell_get_dict_dirs(me);
 	auto dicts = vector<filesystem::path>();
 	nuspell::search_dirs_for_dicts(dirs, dicts);
-	if (empty(dicts)) {
-		*out_n_dicts = 0;
-		return nullptr;
-	}
 	for (auto& d : dicts)
 		d = d.stem();
 	sort(begin(dicts), end(dicts));
@@ -168,11 +164,14 @@ nuspell_provider_list_dicts(EnchantProvider* me,
 	dicts.erase(it, end(dicts));
 
 	char** dictionary_list = g_new0(char*, size(dicts) + 1);
-	transform(begin(dicts), end(dicts), dictionary_list,
-	          [](const filesystem::path& p) {
-		          return g_strdup(p.string().c_str());
-	          });
-	*out_n_dicts = size(dicts);
+	if (dictionary_list) {
+		transform(begin(dicts), end(dicts), dictionary_list,
+			  [](const filesystem::path& p) {
+				  return g_strdup(p.string().c_str());
+			  });
+		*out_n_dicts = size(dicts);
+	} else
+		*out_n_dicts = 0;
 	return dictionary_list;
 }
 

providers/enchant_voikko.c
@@ -73,11 +73,12 @@ voikko_dict_suggest (EnchantDict * me, const char *const word,
 	for (*out_n_suggs = 0; voikko_sugg_arr[*out_n_suggs] != NULL; (*out_n_suggs)++)
 		;
 
-	char **sugg_arr = calloc(sizeof (char *), *out_n_suggs + 1);
-	if (sugg_arr == NULL)
-		return NULL;
-	for (size_t i = 0; i < *out_n_suggs; i++)
-		sugg_arr[i] = strdup (voikko_sugg_arr[i]);
+	char **sugg_arr = g_new0(char *, *out_n_suggs + 1);
+	if (sugg_arr)
+		for (size_t i = 0; i < *out_n_suggs; i++)
+			sugg_arr[i] = strdup (voikko_sugg_arr[i]);
+	else
+		*out_n_suggs = 0;
 	voikkoFreeCstrArray (voikko_sugg_arr);
 	return sugg_arr;
 }
@@ -100,10 +101,12 @@ voikko_provider_list_dicts (EnchantProvider * me, size_t * out_n_dicts)
 		(*out_n_dicts)++;
 
 	char ** out_list = g_new0 (char *, *out_n_dicts + 1);
-	for (size_t i = 0; i < *out_n_dicts; i++)
-		out_list[i] = strdup (voikko_langs[i]);
+	if (out_list)
+		for (size_t i = 0; i < *out_n_dicts; i++)
+			out_list[i] = strdup (voikko_langs[i]);
+	else
+		*out_n_dicts = 0;
 	voikkoFreeCstrArray(voikko_langs);
-
 	return out_list;
 }
 

providers/enchant_zemberek.cpp
@@ -1,5 +1,6 @@
 /* Copyright (C) 2006 Barış Metin <baris@pardus.org.tr>
  * Copyright (C) 2007 Serkan Kaba <serkan_kaba@yahoo.com>
+ * Copyright (C) 2024-2025 Reuben Thomas
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -217,9 +218,13 @@ zemberek_provider_list_dicts (EnchantProvider * me _GL_UNUSED,
     }
   else
     {
-        *out_n_dicts = 1;
+        *out_n_dicts = 0;
         char ** out_list = g_new0 (char *, 2);
-        out_list[0] = g_strdup ("tr");
+        if (out_list) {
+          out_list[0] = g_strdup ("tr");
+          if (out_list[0])
+            *out_n_dicts = 1;
+        }
 
         return out_list;
     }

commit 8ab0d5524dceecb34f6af4e59389a3e2ad3a81b9

Fix hunspell provider returning no results
This bug was introduced in commit 372deba, version 2.8.7.

providers/enchant_hunspell.cpp
@@ -162,6 +162,7 @@ HunspellChecker::suggestWord(const char* const utf8Word, size_t len, size_t *nsu
 		return nullptr;
 
 	std::vector<std::string> sugMS = hunspell->suggest(out);
+	*nsug = sugMS.size();
 	g_free(out);
 	char **sug = g_new0 (char *, *nsug + 1);
 	if (sug) {