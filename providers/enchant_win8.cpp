4c1b4095aae3ea5e5fe033c00ad8909b7ff2d69a
Kneemund <niemand@ymir-graphics.com>
2026-01-26 16:11:06 +0100

    refactor: use bcp47 conversion from gnulib, tabs as indentation

--- providers/enchant_win8.cpp
+++ providers/enchant_win8.cpp
@@ -12,8 +12,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -26,6 +26,7 @@
 
 #include "config.h"
 
+#include <bcp47.h>
 #include <glib.h>
 #include <spellcheck.h>
 
@@ -33,56 +34,69 @@
 
 /* --------- Utils ----------*/
 
+// convert bcp47 tags (e.g. "en-US") to xpg tags (e.g. "en_US")
 static char*
-utf16_to_utf8(const wchar_t* const str, gboolean from_bcp47)
+tag_bcp47_to_xpg(const char* const bcp47)
 {
-    char* utf8 = g_utf16_to_utf8((gunichar2*)str, -1, nullptr, nullptr, nullptr);
-    if (utf8 && from_bcp47) {
-        char* p = utf8;
-        // bcp47 tags use syntax "en-US" while the myspell versions are "en_US"
-        while (*p) {
-            if (*p == '-')
-                *p = '_';
-            p++;
-        }
-    }
-    return utf8;
+	if (!bcp47) {
+		return nullptr;
+	}
+
+	char* xpg = g_new0(char, BCP47_MAX);
+	if (!xpg) {
+		return nullptr;
+	}
+
+	bcp47_to_xpg(xpg, bcp47, nullptr);
+	return xpg;
 }
 
-static wchar_t*
-utf8_to_utf16(const char* const str, gssize len, gboolean to_bcp47)
+// convert xpg tags (e.g. "en_US") to bcp47 tags (e.g. "en-US")
+static char*
+tag_xpg_to_bcp47(const char* const xpg)
 {
-    wchar_t* utf16 = (wchar_t*)g_utf8_to_utf16(str, (glong)len, nullptr, nullptr, nullptr);
-    if (utf16 && to_bcp47) {
-        wchar_t* p = utf16;
-        // bcp47 tags use syntax "en-US" while the myspell versions are "en_US"
-        while (*p) {
-            if (*p == L'_')
-                *p = L'-';
-            p++;
-        }
-    }
-    return utf16;
+	if (!xpg) {
+		return nullptr;
+	}
+
+	char* bcp47 = g_new0(char, BCP47_MAX);
+	if (!bcp47) {
+		return nullptr;
+	}
+
+	xpg_to_bcp47(bcp47, xpg);
+	return bcp47;
 }
 
 static char**
-enumstring_to_chararray(IEnumString* strings, size_t* out_len, gboolean from_bcp47)
+enumstring_to_chararray(IEnumString* strings, size_t* out_len, gboolean tags_from_bcp47)
 {
-    GArray* array = g_array_new(TRUE, FALSE, sizeof(char*));
-    LPOLESTR wstr = nullptr;
+	GArray* array = g_array_new(TRUE, FALSE, sizeof(char*));
 
-    while (SUCCEEDED(strings->Next(1, &wstr, nullptr)) && wstr) {
-        char* str = utf16_to_utf8(wstr, from_bcp47);
-        if (str) {
-            g_array_append_val(array, str);
-        }
-        CoTaskMemFree(wstr);
-    }
-    strings->Release();
+	LPOLESTR w_str;
+	while (SUCCEEDED(strings->Next(1, &w_str, nullptr)) && w_str) {
+		char* str = g_utf16_to_utf8((gunichar2*)w_str, -1, nullptr, nullptr, nullptr);
 
-    *out_len = array->len;
-    char** result = (char**)g_array_free(array, FALSE);
-    return result;
+		if (str) {
+			if (tags_from_bcp47) {
+				char* xpg_tag = tag_bcp47_to_xpg(str);
+				g_free(str);
+
+				if (xpg_tag) {
+					g_array_append_val(array, xpg_tag);
+				}
+			} else {
+				g_array_append_val(array, str);
+			}
+		}
+
+		CoTaskMemFree(w_str);
+	}
+
+	strings->Release();
+
+	*out_len = array->len;
+	return (char**)g_array_free(array, FALSE);
 }
 
 /* ---------- Dict ------------ */
@@ -90,161 +104,178 @@ enumstring_to_chararray(IEnumString* strings, size_t* out_len, gboolean from_bcp
 static void
 win8_dict_add_to_session(EnchantProviderDict* dict, const char* const word, size_t len)
 {
-    auto checker = static_cast<ISpellChecker*>(dict->user_data);
-    wchar_t* wword = utf8_to_utf16(word, len, FALSE);
+	auto checker = static_cast<ISpellChecker*>(dict->user_data);
+	LPCWSTR w_word = (LPCWSTR)g_utf8_to_utf16(word, (glong)len, nullptr, nullptr, nullptr);
 
-    if (wword) {
-        checker->Add(wword);
-        g_free(wword);
-    }
+	if (w_word) {
+		checker->Add(w_word);
+		g_free(w_word);
+	}
 }
 
 static void
 win8_dict_remove_from_session(EnchantProviderDict* dict, const char* const word, size_t len)
 {
-    auto checker = static_cast<ISpellChecker*>(dict->user_data);
+	auto checker = static_cast<ISpellChecker*>(dict->user_data);
 
-    // try to use ISpellChecker2::Remove if available (Windows 10+)
-    ISpellChecker2* checker2 = nullptr;
-    if (SUCCEEDED(checker->QueryInterface(__uuidof(ISpellChecker2), (void**)&checker2))) {
-        wchar_t* wword = utf8_to_utf16(word, len, FALSE);
+	// try to use ISpellChecker2::Remove if available (Windows 10+)
+	ISpellChecker2* checker2;
+	if (SUCCEEDED(checker->QueryInterface(__uuidof(ISpellChecker2), (void**)&checker2))) {
+		LPCWSTR w_word = (LPCWSTR)g_utf8_to_utf16(word, (glong)len, nullptr, nullptr, nullptr);
 
-        if (wword) {
-            checker2->Remove(wword);
-            g_free(wword);
-        }
+		if (w_word) {
+			checker2->Remove(w_word);
+			g_free(w_word);
+		}
 
-        checker2->Release();
-    }
+		checker2->Release();
+	}
 }
 
 static int
 win8_dict_check(EnchantProviderDict* dict, const char* const word, size_t len)
 {
-    auto checker = static_cast<ISpellChecker*>(dict->user_data);
-    wchar_t* wword = utf8_to_utf16(word, len, FALSE);
-    IEnumSpellingError* errors;
-    ISpellingError* error = nullptr;
-    HRESULT hr;
+	auto checker = static_cast<ISpellChecker*>(dict->user_data);
+	LPCWSTR w_word = (LPCWSTR)g_utf8_to_utf16(word, (glong)len, nullptr, nullptr, nullptr);
 
-    if (!wword) {
-        return -1; // conversion error
-    }
+	if (!w_word) {
+		return -1; // conversion error
+	}
 
-    hr = checker->Check(wword, &errors);
-    g_free(wword);
+	IEnumSpellingError* errors;
+	HRESULT hr = checker->Check(w_word, &errors);
+	g_free(w_word);
 
-    if (FAILED(hr)) {
-        return -1; // error
-    }
+	if (FAILED(hr)) {
+		return -1; // error
+	}
 
-    if (errors->Next(&error) == S_OK) {
-        error->Release();
-        errors->Release();
-        return 1; // spelling issue
-    }
-    
-    errors->Release();
-    return 0; // correct
+	ISpellingError* error;
+	if (errors->Next(&error) == S_OK) {
+		error->Release();
+		errors->Release();
+		return 1; // spelling issue
+	}
+
+	errors->Release();
+	return 0; // correct
 }
 
 static char**
 win8_dict_suggest(EnchantProviderDict* dict, const char* const word, size_t len, size_t* out_n_suggs)
 {
-    auto checker = static_cast<ISpellChecker*>(dict->user_data);
-    wchar_t* wword = utf8_to_utf16(word, len, FALSE);
-    IEnumString* suggestions;
-    HRESULT hr;
+	auto checker = static_cast<ISpellChecker*>(dict->user_data);
+	LPCWSTR w_word = (LPCWSTR)g_utf8_to_utf16(word, (glong)len, nullptr, nullptr, nullptr);
 
-    if (!wword) {
-        *out_n_suggs = 0;
-        return nullptr;
-    }
+	if (!w_word) {
+		*out_n_suggs = 0;
+		return nullptr;
+	}
 
-    hr = checker->Suggest(wword, &suggestions);
-    g_free(wword);
+	IEnumString* suggestions;
+	HRESULT hr = checker->Suggest(w_word, &suggestions);
+	g_free(w_word);
 
-    if (FAILED(hr)) {
-        *out_n_suggs = 0;
-        return nullptr;
-    }
+	if (FAILED(hr)) {
+		*out_n_suggs = 0;
+		return nullptr;
+	}
 
-    return enumstring_to_chararray(suggestions, out_n_suggs, FALSE);
+	return enumstring_to_chararray(suggestions, out_n_suggs, FALSE);
 }
 
 /* ---------- Provider ------------ */
 
 static EnchantProviderDict*
-win8_provider_request_dict(EnchantProvider* provider, const char* const tag)
+win8_provider_request_dict(EnchantProvider* provider, const char* const xpg_tag)
 {
-    auto factory = static_cast<ISpellCheckerFactory*>(provider->user_data);
-    ISpellChecker* checker;
-    EnchantProviderDict* dict;
-    wchar_t* wtag = utf8_to_utf16(tag, -1, TRUE);
-    HRESULT hr;
+	auto factory = static_cast<ISpellCheckerFactory*>(provider->user_data);
 
-    hr = factory->CreateSpellChecker(wtag, &checker);
-    g_free(wtag);
+	char* bcp47_tag = tag_xpg_to_bcp47(xpg_tag);
+	if (!bcp47_tag) {
+		return nullptr;
+	}
 
-    if (FAILED(hr)) {
-        return nullptr;
-    }
+	LPCWSTR w_bcp47_tag = (LPCWSTR)g_utf8_to_utf16(bcp47_tag, -1, nullptr, nullptr, nullptr);
+	g_free(bcp47_tag);
+	if (!w_bcp47_tag) {
+		return nullptr;
+	}
 
-    dict = enchant_provider_dict_new(provider, tag);
-    dict->suggest = win8_dict_suggest;
-    dict->check = win8_dict_check;
-    dict->add_to_session = win8_dict_add_to_session;
-    dict->remove_from_session = win8_dict_remove_from_session;
+	ISpellChecker* checker;
+	HRESULT hr = factory->CreateSpellChecker(w_bcp47_tag, &checker);
+	g_free(w_bcp47_tag);
 
-    dict->user_data = checker;
+	if (FAILED(hr)) {
+		return nullptr;
+	}
 
-    return dict;
+	EnchantProviderDict* dict = enchant_provider_dict_new(provider, xpg_tag);
+	dict->user_data = checker;
+
+	dict->suggest = win8_dict_suggest;
+	dict->check = win8_dict_check;
+	dict->add_to_session = win8_dict_add_to_session;
+	dict->remove_from_session = win8_dict_remove_from_session;
+
+	return dict;
 }
 
 static void
 win8_provider_dispose_dict(_GL_UNUSED EnchantProvider* provider, EnchantProviderDict* dict)
 {
-    if (dict) {
-        auto checker = static_cast<ISpellChecker*>(dict->user_data);
-        checker->Release();
-    }
+	if (dict) {
+		auto checker = static_cast<ISpellChecker*>(dict->user_data);
+		checker->Release();
+	}
 }
 
 static int
-win8_provider_dictionary_exists(EnchantProvider* provider, const char* const tag)
+win8_provider_dictionary_exists(EnchantProvider* provider, const char* const xpg_tag)
 {
-    auto factory = static_cast<ISpellCheckerFactory*>(provider->user_data);
-    wchar_t* wtag = utf8_to_utf16(tag, -1, TRUE);
+	auto factory = static_cast<ISpellCheckerFactory*>(provider->user_data);
 
-    BOOL is_supported = FALSE;
-    factory->IsSupported(wtag, &is_supported);
+	char* bcp47_tag = tag_xpg_to_bcp47(xpg_tag);
+	if (!bcp47_tag) {
+		return 0;
+	}
 
-    g_free(wtag);
-    return is_supported;
+	LPCWSTR w_bcp47_tag = (LPCWSTR)g_utf8_to_utf16(bcp47_tag, -1, nullptr, nullptr, nullptr);
+	g_free(bcp47_tag);
+	if (!w_bcp47_tag) {
+		return 0;
+	}
+
+	BOOL is_supported = FALSE;
+	factory->IsSupported(w_bcp47_tag, &is_supported);
+	g_free(w_bcp47_tag);
+
+	return is_supported;
 }
 
 static char**
 win8_provider_list_dicts(EnchantProvider* provider, size_t* out_n_dicts)
 {
-    auto factory = static_cast<ISpellCheckerFactory*>(provider->user_data);
-    IEnumString* dicts;
+	auto factory = static_cast<ISpellCheckerFactory*>(provider->user_data);
 
-    if (FAILED(factory->get_SupportedLanguages(&dicts))) {
-        *out_n_dicts = 0;
-        return nullptr;
-    }
+	IEnumString* dicts;
+	if (FAILED(factory->get_SupportedLanguages(&dicts))) {
+		*out_n_dicts = 0;
+		return nullptr;
+	}
 
-    return enumstring_to_chararray(dicts, out_n_dicts, TRUE);
+	return enumstring_to_chararray(dicts, out_n_dicts, TRUE);
 }
 
 static void
 win8_provider_dispose(EnchantProvider* provider)
 {
-    if (provider) {
-        auto factory = static_cast<ISpellCheckerFactory*>(provider->user_data);
-        factory->Release();
-    }
-    CoUninitialize();
+	if (provider) {
+		auto factory = static_cast<ISpellCheckerFactory*>(provider->user_data);
+		factory->Release();
+	}
+
+	CoUninitialize();
 }
 
 static const char*
@@ -265,35 +296,30 @@ init_enchant_provider(void);
 EnchantProvider*
 init_enchant_provider(void)
 {
-    EnchantProvider* provider;
-    ISpellCheckerFactory* factory;
+	// Initialize COM (required before using SpellCheckerFactory)
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+	if (FAILED(hr) && hr != RPC_E_CHANGED_MODE) {
+		// RPC_E_CHANGED_MODE means COM was already initialized with a different
+		// threading model, which is fine - we can still use it
+		return nullptr;
+	}
 
-    // Initialize COM (required before using SpellCheckerFactory)
-    HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
-    if (FAILED(hr) && hr != RPC_E_CHANGED_MODE) {
-        // RPC_E_CHANGED_MODE means COM was already initialized with a different
-        // threading model, which is fine - we can still use it
-        return nullptr;
-    }
+	ISpellCheckerFactory* factory;
+	if (FAILED(CoCreateInstance(__uuidof(SpellCheckerFactory), nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&factory)))) {
+		CoUninitialize();
+		return nullptr;
+	}
 
-    if (FAILED(CoCreateInstance(__uuidof(SpellCheckerFactory),
-                                nullptr,
-                                CLSCTX_INPROC_SERVER,
-                                IID_PPV_ARGS(&factory)))) {
-        CoUninitialize();
-        return nullptr;
-    }
+	EnchantProvider* provider = enchant_provider_new();
+	provider->user_data = factory;
 
-    provider = enchant_provider_new();
-    provider->dispose = win8_provider_dispose;
-    provider->request_dict = win8_provider_request_dict;
-    provider->dispose_dict = win8_provider_dispose_dict;
-    provider->dictionary_exists = win8_provider_dictionary_exists;
-    provider->identify = win8_provider_identify;
-    provider->describe = win8_provider_describe;
-    provider->list_dicts = win8_provider_list_dicts;
+	provider->dispose = win8_provider_dispose;
+	provider->request_dict = win8_provider_request_dict;
+	provider->dispose_dict = win8_provider_dispose_dict;
+	provider->dictionary_exists = win8_provider_dictionary_exists;
+	provider->identify = win8_provider_identify;
+	provider->describe = win8_provider_describe;
+	provider->list_dicts = win8_provider_list_dicts;
 
-    provider->user_data = factory;
-
-    return provider;
+	return provider;
 }
